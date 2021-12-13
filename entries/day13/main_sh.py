e1fc2982d6e2672e65e89d5bea516f6ced90bd1d
Patrick <patrickjung@rogers.com>
2021-12-13 01:58:39 -0500

    Day 13 solutions

---
+++ entries/day13/main_sh.py
@@ -0,0 +1,26 @@
+if __name__ == "__main__":
+    SIZE = 2000
+
+    fold_y, fold_x = lambda p: (2 * magn - p[0], p[1]), lambda p: (p[0], 2 * magn - p[1])
+
+    with open('input.txt', 'r') as f:
+        coords, instructs_raw = [x.split('\n') for x in f.read().strip().split('\n\n')]
+
+        points = {(coord_r, coord_c) for coord_c, coord_r in map(lambda s: map(int, s.split(',')), coords)}
+        instructs = ((lambda l, r: (l[-1] == 'y', int(r)))(*instruct.split('=')) for instruct in instructs_raw)
+
+        for symb_bool, magn in instructs:
+            points_f = set(filter(lambda p: p[not symb_bool] >= magn, points))
+            points_n = set(map((fold_x, fold_y)[symb_bool], points_f))
+            points = (points - points_f).union(points_n)
+
+        grid = [[' ' for _i in range(SIZE)] for _j in range(SIZE)]
+        for r, c in points:
+            grid[r][c] = '#'
+
+        for r in grid:
+            line_res = ''.join(r).strip()
+            if line_res:
+                print(line_res)
+            else:
+                break
